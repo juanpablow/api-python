11871666cc618ef25c1e89765199afb913decc59
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-01 23:39:20 -0300

    add swagger

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
@@ -6,3 +7,19 @@ app = FastAPI()
 @app.get("/")
 async def read_main():
 	return {"msg": "Hello World"}
+
+
+def custom_openapi():
+	if app.openapi_schema:
+		return app.openapi_schema
+	openapi_schema = get_openapi(
+		title="TaskAPI",
+		version="1.0.0",
+		description="Project to add tasks",
+		routes=app.routes,
+	)
+	app.openapi_schema = openapi_schema
+	return app.openapi_schema
+
+
+app.openapi = custom_openapi
