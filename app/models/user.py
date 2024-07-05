9a1e403f65bfd09523d0da39237edb13a36f992b
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-04 22:45:36 -0300

    refactor model `User`

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,16 @@
-from uuid import UUID
-from pydantic import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID as SQLAlchemyUUID
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+Base = declarative_base()
 
 
-class User(BaseModel):
-	id: UUID
-	name: str
-	email: str
-	password: str
+class User(Base):
+	__tablename__ = "users"
+	id = Column(
+		SQLAlchemyUUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+	)
+	name = Column(String, nullable=False)
+	# email = Column(String, nullable=False, unique=True)
+	# password = Column(String, nullable=False)
